a38a8bc362ac0bf52666b9cee48e6e012a75d7c1
baeac <baeacbusiness@gmail.com>
2025-07-25 02:05:14 +0200

    Update __init__.py

--- ps1_ify/__init__.py
+++ ps1_ify/__init__.py
@@ -4,8 +4,8 @@ from ps1_ify import ps1_ify
 bl_info = {
     "name": "PS1-ify",
     "author": "baeac",
-    "version" : (1, 4, 0),
-    "blender" : (4, 2, 0),
+    "version" : (1, 5, 0),
+    "blender" : (4, 5, 0),
     "location" : "View3D > PS1",
     "category" : "Import Settings",
     "description" : "Easily import all the settings needed to make your renders look like a PS1 game.",
